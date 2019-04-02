693c3a971d94fb83d2557c1cb1ca86a78c47aa32
Arya Wiranata <wiranata.arya.wiranata@gmail.com>
2019-04-02 16:23:10 +0700

    coba branch

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
     res.render('home');
 });
+app.get('/:name', function (req, res) {
+    res.render(req.params.name);
+});
 
 app.listen(3000);
